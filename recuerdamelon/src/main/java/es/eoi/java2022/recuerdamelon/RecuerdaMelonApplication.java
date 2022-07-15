016a5c82c4072ab476639851646b0edbdcfa93ab
Enrique Mingorance Cano <ginxaco@gmail.com>
2022-07-15 13:40:34 +0200

    @EnableAutoconfiguration removed

--- recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/RecuerdaMelonApplication.java
+++ recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/RecuerdaMelonApplication.java
@@ -10,18 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class RecuerdaMelonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecuerdaMelonApplication.class, args);
 	}
 
-	@PostConstruct
-	public void init(){
-		System.out.println("Hola mundo desde RecuerdaMelonApplication");
-	}
-
 }
-//@EntityScan("es.eoi.java2022.recuerdamelon.data.entity")
-//@EnableJpaRepositories("es.eoi.java2022.recuerdamelon.data.repository")
