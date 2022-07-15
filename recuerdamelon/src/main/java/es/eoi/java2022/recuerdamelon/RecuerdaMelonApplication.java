5fbead00cac5818842efdde0b3abe29ba29f8eae
Enrique Mingorance Cano <ginxaco@gmail.com>
2022-07-15 13:45:03 +0200

    yoqueseque

--- recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/RecuerdaMelonApplication.java
+++ recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/RecuerdaMelonApplication.java
@@ -1,13 +1,7 @@
 package es.eoi.java2022.recuerdamelon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RecuerdaMelonApplication {
