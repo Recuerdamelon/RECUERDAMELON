646ccfae307ab1d872bf5c10363357e03a044db7
juanc <juancarrascoguerrero@gmail.com>
2022-07-22 14:44:35 +0200

    perfil.html added

--- recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/web/HomeController.java
+++ recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/web/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/perfil")
+    public String perfil() {
+        return "perfil";
+    }
 }
