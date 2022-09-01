7ca357dea5f52f190e9d52c0e6d42f03ec1c2c10
juancarrascoguerrero <juancarrascoguerrero@gmail.com>
2022-09-01 19:29:13 +0200

    chANGES at motorhome

--- recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/web/LoginController.java
+++ recuerdamelon/src/main/java/es/eoi/java2022/recuerdamelon/web/LoginController.java
@@ -152,7 +152,7 @@ public class LoginController {
         } else {
             userDTO.setPassword(encoder.encode(userDTO.getPassword()));
             userDTO.setActive(true);
-            userDTO.setRoles(SetRoleToUser.setRole(1));
+            userDTO.setRoles(SetRoleToUser.setRole(1, userRoleService, userRoleServiceMapper));
             this.userRepository.save(userService.save(userDTO));
             return "login";
         }
@@ -183,7 +183,7 @@ public class LoginController {
             userDTO.setName("business");
             userDTO.setSurname("business");
 
-            userDTO.setRoles(SetRoleToUser.setRole(3));
+            userDTO.setRoles(SetRoleToUser.setRole(3, userRoleService, userRoleServiceMapper));
 
             this.userRepository.save(userService.save(userDTO));
             return "login";
